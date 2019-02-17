29d74f1fad564dea5e98a67bc7dd05a96cdf6fbb
hepeng <1552278784@qq.com>
2019-02-17 21:31:00 +0800

    first commit

---
+++ src/Enmu/TestWeek.java
@@ -0,0 +1,10 @@
+package Enmu;
+
+public class TestWeek {
+    public static void main(String[] args) {
+        Week mon=Week.MON;
+        Week tue=Week.TUE;
+        Week wes=Week.WES;
+        mon.show();
+    }
+}
---
+++ src/Enmu/Week.java
@@ -0,0 +1,31 @@
+package Enmu;
+
+public abstract class Week {
+    public static final Week MON=new Week("星期一"){
+        @Override
+        public void show() {
+            System.out.println("星期一");
+        }
+    };
+    public static final Week TUE=new Week("星期二"){
+        public void show() {
+            System.out.println("星期二");
+        }
+    };
+    public static final Week WES=new Week("星期三"){
+        public void show() {
+            System.out.println("星期三");
+        }
+    };
+    private String name;
+    private Week(String name){
+        this.name=name;
+    }
+    public abstract void show();
+    @Override
+    public String toString() {
+        return "Week{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
